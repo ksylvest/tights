08d424feba98803cb0831b8a2e4cb04e4d504fa9
Kevin Sylvestre <kevin@ksylvest.com>
2019-09-02 09:14:05 -0700

    Fix type for src support has.tsx

--- src/support/has.tsx
+++ src/support/has.tsx
@@ -1,5 +1,7 @@
 import { property } from "./property";
 
+type Name = "addons" | "dropdown";
+
 export const has = (Component: React.FC, name: Name) => {
   property(name, Component, { [name]: !!name }, `.has-${name}`);
 };
