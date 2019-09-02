aaf16012443e802ebf9f0ac060fe94a2813d9640
Kevin Sylvestre <kevin@ksylvest.com>
2019-09-02 11:17:21 -0700

    Integrate JSX storybook addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,9 @@
-import { configure } from "@storybook/react";
+import { configure, addDecorator } from "@storybook/react";
+import { jsxDecorator } from 'storybook-addon-jsx';
 
 import "bulma/css/bulma.css";
+
+addDecorator(jsxDecorator);
 
 configure(() => {
   require("../src/stories/index.ts");
