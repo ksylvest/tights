ac9cb6bdd1aab5ec201528e5277124d198e16f56
Kevin Sylvestre <kevin@ksylvest.com>
2019-09-07 12:36:51 -0700

    Delete duplication in card / column stories

--- src/stories/columns.tsx
+++ src/stories/columns.tsx
@@ -5,22 +5,22 @@ import { Columns } from "../components";
 import { Column } from "../components";
 import { Image } from "../components";
 
-const SAMPLES = [
-  { unsplashID: "aj5ZgaE2Gi0" },
-  { unsplashID: "xEY0dtFfon0" },
-  { unsplashID: "uYcEkcREK7E" },
-  { unsplashID: "kn7FVkDwLfU" },
-  { unsplashID: "2TK6cWlqiyU" },
-  { unsplashID: "6qIXonfBg1E" },
-  { unsplashID: "brKCHU0Xo5s" },
-  { unsplashID: "4gNhkdomIRc" },
+const UNSPLASH_IDS = [
+  "aj5ZgaE2Gi0",
+  "xEY0dtFfon0",
+  "uYcEkcREK7E",
+  "kn7FVkDwLfU",
+  "2TK6cWlqiyU",
+  "6qIXonfBg1E",
+  "brKCHU0Xo5s",
+  "4gNhkdomIRc",
 ];
 
 const stories = storiesOf("Columns", module);
 
 stories.add("Basic", () => (
   <Columns multiline mobile tablet desktop>
-    {SAMPLES.map(({ unsplashID }, key) => (
+    {UNSPLASH_IDS.map((unsplashID, key) => (
       <Column key={key} mobile={6} tablet={4} desktop={3}>
         <Image src={`https://source.unsplash.com/${unsplashID}/800x800`} />
       </Column>
