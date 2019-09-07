613e99787a6738666182ce88e0caa17ca113a02e
Kevin Sylvestre <kevin@ksylvest.com>
2019-09-07 12:53:28 -0700

    Fix class name proxy bug for tile / box

--- src/components/box/index.tsx
+++ src/components/box/index.tsx
@@ -1,6 +1,8 @@
 import cn from "classnames";
 import * as React from "react";
 
-export const Box: React.FC = (className, ...props) => (
+export const Box: React.FC<{
+  className?: string;
+}> = ({ className, ...props }) => (
   <div {...props} className={cn("box", className)} />
 );
--- src/components/tile/index.tsx
+++ src/components/tile/index.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 type Size = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
 export const Tile: React.FC<{
-  tag?: React.FC<{ className: string }>;
+  tag?: React.FC<{ className?: string }>;
   ancestor?: boolean;
   parent?: boolean;
   child?: boolean;
