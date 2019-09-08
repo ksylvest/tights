dc2192231efc875b14da427ea1b54069a60325bc
Kevin Sylvestre <kevin@ksylvest.com>
2019-09-08 13:37:11 -0700

    Refactor duplicating tags

--- src/components/title/index.tsx
+++ src/components/title/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 
 export const Title: React.FC<{
-  tag?: "p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  tag?: React.FC<{ className?: string }>;
 }> = ({ tag: Tag = "p", ...props }) => <Tag {...props} className="title" />;
