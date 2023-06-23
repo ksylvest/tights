6c39ab989c8a6629a64ed593ac4216b7342fdc06
Kevin Sylvestre <kevin@ksylvest.com>
2023-06-22 17:39:02 -0700

    Allow for custom tag on NavbarItem
    Can be either a or div per docs.

--- src/navbar_item.tsx
+++ src/navbar_item.tsx
@@ -1,23 +1,33 @@
 import { clsx } from "clsx";
-import type { ComponentProps, FC } from "react";
+import type { ComponentProps, ElementType } from "react";
 
-type Props = {
+type Props<T extends ElementType> = {
+  tag?: T;
   dropdown?: boolean;
   hoverable?: boolean;
   active?: boolean;
 };
 
-export const NavbarItem: FC<
-  Omit<ComponentProps<"div">, keyof Props> & Props
-> = ({ dropdown, hoverable, active, className, ...props }) => (
-  <div
-    {...props}
-    className={clsx(
-      "navbar-item",
-      hoverable && "is-hoverable",
-      active && "is-active",
-      dropdown && "has-dropdown",
-      className
-    )}
-  />
-);
+export const NavbarItem = <T extends ElementType = "div">({
+  dropdown,
+  hoverable,
+  active,
+  tag,
+  className,
+  ...props
+}: Omit<ComponentProps<T>, keyof Props<T>> & Props<T>) => {
+  const Tag = tag ?? "div";
+
+  return (
+    <Tag
+      {...props}
+      className={clsx(
+        "navbar-item",
+        hoverable && "is-hoverable",
+        active && "is-active",
+        dropdown && "has-dropdown",
+        className
+      )}
+    />
+  );
+};
