bbbe2f9a601065089c86b671bb5d10d586c60a14
Kevin Sylvestre <kevin@ksylvest.com>
2023-07-17 13:55:24 -0700

    Size for Title / Subtitle components

--- src/title.tsx
+++ src/title.tsx
@@ -3,13 +3,27 @@ import type { ComponentProps, ElementType, FC } from "react";
 
 type Props<T extends ElementType> = {
   tag?: T;
+  size?: 1 | 2 | 3 | 4 | 5 | 6;
+  spaced?: boolean;
 };
 
 export const Title = <T extends ElementType = "p">({
   className,
   tag,
+  size,
+  spaced,
   ...props
 }: Props<T> & Omit<ComponentProps<T>, keyof Props<T>>) => {
   const Tag = tag ?? "p";
-  return <Tag {...props} className={clsx("title", className)} />;
+  return (
+    <Tag
+      {...props}
+      className={clsx(
+        "title",
+        size && `is-${size}`,
+        spaced && "is-spaced",
+        className,
+      )}
+    />
+  );
 };
