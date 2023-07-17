6ce5eba65daed59a69672631b131585e43f7884f
Kevin Sylvestre <kevin@ksylvest.com>
2023-07-17 13:02:33 -0700

    IconText component for rendering text w/ icon

---
+++ src/icon_text.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRocket } from "@fortawesome/free-solid-svg-icons";
+
+import { Icon } from "./icon";
+import { IconText } from "./icon_text";
+
+const meta = {
+  title: "IconText",
+  component: Icon,
+  tags: ["autodocs"],
+} satisfies Meta<typeof Icon>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const BasicStory: Story = {
+  render: (args) => (
+    <IconText>
+      <Icon {...args}>
+        <FontAwesomeIcon icon={faRocket} />
+      </Icon>
+      <span>Rocket!</span>
+    </IconText>
+  ),
+};
